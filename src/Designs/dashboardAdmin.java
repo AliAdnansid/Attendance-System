b4327f9d7426ee1f89011790044ff2c27ed4f9da
Aliadnansid <xoxoidgafayxoxo@gmail.com>
2024-08-26 22:07:38 +0500

    Update complete

--- src/Designs/dashboardAdmin.java
+++ src/Designs/dashboardAdmin.java
@@ -33,7 +33,7 @@ public class dashboardAdmin extends javax.swing.JFrame {
         addTB = new javax.swing.JButton();
         deleteTB = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        updateB = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jButton6 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
@@ -89,10 +89,15 @@ public class dashboardAdmin extends javax.swing.JFrame {
         jButton4.setForeground(new java.awt.Color(0, 0, 0));
         jButton4.setText("SEARCH");
 
-        jButton5.setBackground(new java.awt.Color(255, 255, 255));
-        jButton5.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
-        jButton5.setForeground(new java.awt.Color(0, 0, 0));
-        jButton5.setText("UPDATE");
+        updateB.setBackground(new java.awt.Color(255, 255, 255));
+        updateB.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        updateB.setForeground(new java.awt.Color(0, 0, 0));
+        updateB.setText("UPDATE");
+        updateB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                updateBActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -106,7 +111,7 @@ public class dashboardAdmin extends javax.swing.JFrame {
                 .addGap(30, 30, 30)
                 .addComponent(jButton4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton5)
+                .addComponent(updateB)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -117,7 +122,7 @@ public class dashboardAdmin extends javax.swing.JFrame {
                     .addComponent(addTB, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(deleteTB, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(updateB, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(30, Short.MAX_VALUE))
         );
 
@@ -345,6 +350,13 @@ public class dashboardAdmin extends javax.swing.JFrame {
         new deleteTeacher().setVisible(true);
     }//GEN-LAST:event_deleteTBActionPerformed
 
+    private void updateBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateBActionPerformed
+        // TODO add your handling code here:
+        this.setVisible(false);
+        this.dispose();
+        new updateTeacher().setVisible(true);
+    }//GEN-LAST:event_updateBActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -391,7 +403,6 @@ public class dashboardAdmin extends javax.swing.JFrame {
     private javax.swing.JButton jButton12;
     private javax.swing.JButton jButton13;
     private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
@@ -404,5 +415,6 @@ public class dashboardAdmin extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
+    private javax.swing.JButton updateB;
     // End of variables declaration//GEN-END:variables
 }
